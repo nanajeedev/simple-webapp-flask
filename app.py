7203125474b412a6a828b6f8f4fda32cb72cd695
nanajeedev <mamidinanajee@gmail.com>
2023-10-07 11:54:58 +0530

    docker file added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+ls import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -9,6 +9,10 @@ def main():
 @app.route('/how are you')
 def hello():
     return 'I am good, how about you?'
+     
+@app.route('/hello')
+def hello2():
+    return 'my name is MAMIDI NANAJEE'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
