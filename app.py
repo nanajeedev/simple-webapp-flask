ead0a19b9ddcdd43100adddcf5a1eff156ba7e77
nanajeedev <mamidinanajee@gmail.com>
2023-10-10 12:54:44 +0530

    code changed

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ def hello():
 @app.route('/hello')
 def hello2():
     return 'my name is nanajee'
+@app.route('/hai')
+def hello3():
+    return 'WELCOME TO VIZG'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
